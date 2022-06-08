0d68de9fbf4e612345103b040bb08de0e23a8fb8
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-08 22:28:40 +0700

    Challenge-06

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -16,9 +17,45 @@ let modifyFile3 = (val) => {
   file3 = val;
 };
 
-// TODO: Kerjakan bacaData
-// gunakan variabel file1, file2, dan file3
-const bacaData = null;
+//TODO: Kerjakan bacaData
+//gunakan variabel file1, file2, dan file3
+const bacaData = (fnCallback) =>{
+  const arrHasil = [];
+
+  fs.readFile(file1, "utf-8", (err, data1) =>{
+    const arrObj = JSON.parse(data1).message.split(" ");
+
+    if(arrObj.length > 2){
+      fnCallback("Message lebih dari 2 karakter", null);
+      return;
+    }else{
+        arrHasil.push(arrObj[1]);
+    }
+
+    fs.readFile(file2, "utf-8", (err, data2) =>{
+      const arrObj = JSON.parse(data2)[0].message.split(" ");
+      if(arrObj.length > 2){
+        fnCallback("Message lebih dari 2 karakter", null);
+        return;
+      }else{
+        arrHasil.push(arrObj[1]);
+      }
+
+      fs.readFile(file3, "utf-8", (err, data3) =>{
+        const arrObj = JSON.parse(data3)[0].data.message.split(" ");
+        if(arrObj.length > 2){
+          fnCallback("Message lebih dari 2 karakter", null);
+          return;
+        }else{
+          arrHasil.push(arrObj[1]);
+        }
+
+        fnCallback(null, arrHasil);
+
+      });
+    });
+  });
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
